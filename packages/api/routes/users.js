a5ef7b843327792240a9438a149c77fe0f45196b
Christopher Liu <christopherliu830@gmail.com>
2021-02-20 01:10:27 -0500

    user create

--- packages/api/routes/users.js
+++ packages/api/routes/users.js
@@ -1,9 +1,15 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+// eslint-disable-next-line new-cap
+const router = express.Router();
+const UserModel = require('../models/user');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/create', async (req, res, next) => {
+  UserModel.create(req.body)
+      .then(() => {
+        res.status(201).send({message: 'Account successfully created'});
+      })
+      .catch((err) => res.status(500).send(err.errorInfo));
 });
 
 module.exports = router;
