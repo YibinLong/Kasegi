9207997fbb3d7c02ac2a6abede51f0e14277d907
Christopher Liu <christopherliu830@gmail.com>
2021-02-20 18:35:17 -0500

    dashboard layout

--- packages/frontend/src/Pages/Dashboard/index.js
+++ packages/frontend/src/Pages/Dashboard/index.js
@@ -1,13 +1,20 @@
+import { Grid, Container } from '@material-ui/core';
 import React from 'react';
 import { useUser } from '../../api'; 
-import ProfileSidebar from '../../Components/ProfileSidebar';
+import { Wall } from '../../Components/Wall';
 
 export const Dashboard = () => {
   const user = useUser();
   if (!user) return <></>;
   return (
-    <div>
-      <ProfileSidebar />
-    </div>
+    <Container 
+      component="main"
+      maxWidth="md"
+      style={{marginTop: '2rem'}}
+    >
+      <Grid>
+        <Wall />
+      </Grid>
+    </Container>
   )
 }
