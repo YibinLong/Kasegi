30cd172c082f23c8bb7305ef531a81314f2a1c09
Christopher Liu <christopherliu830@gmail.com>
2021-02-21 07:55:43 -0500

    firebase now works when deployed

--- packages/frontend/src/api/init.js
+++ packages/frontend/src/api/init.js
@@ -1,6 +1,3 @@
 import firebase from 'firebase/app';
 import config from '../config';
-
-process.env.NODE_ENV === 'production' ? 
-  firebase.initializeApp() :
-  firebase.initializeApp(config.firebase);
+firebase.initializeApp(config.firebase);
